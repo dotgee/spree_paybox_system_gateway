eafc9c238f967bdbdb9544a24ea3442bb1211d91
Alban Merino <amerino@dotgee.fr>
2014-11-26 12:00:29 +0100

    Upgrade for spree 2-3-stable
    update for spree 2-3-stable

---
+++ config/initializers/payr.rb
@@ -0,0 +1,35 @@
+Payr.setup do |config|
+  # Put the  merchant site ID found on the paybox website
+  #config.site_id = 1999888
+
+  # Put the merchant rang found on the paybox website
+  #config.rang = 32
+
+  # Put the merchant paybox ID found on the paybox website
+  #config.paybox_id = 1686319
+
+  # Put the secret key for the hmac pass found on the paybox website
+  #config.secret_key = "0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF"
+
+  # Put the hash algorithm
+  # Possible values are :SHA256 :SHA512 :SHA384 :SHA224
+  config.hash = :sha512
+
+  # The currency
+  # possible values :euro :us_dollar
+  config.currency = :euro
+
+  # config.paybox_url = "https://preprod-tpeweb.paybox.com/cgi/MYchoix_pagepaiement.cgi"
+  # config.paybox_url_back_one = nil
+  # config.paybox_url_back_two = nil
+
+  config.callback_values = { amount:"m", ref:"r", auto:"a", error:"e", signature:"k" }
+
+  # Optionnal config : if not null, choose on behalf of the user the type of paiement.
+  # EX: "CARTE". Look at the paybox documentation for more
+  #config.typepaiement = "CARTE"
+
+  # Optionnal config : if not null, choose on behalf of the user the type of CARD.
+  # EX: "CB". Look at the paybox documentation for more
+  #config.typecard = "CB"
+end
--- lib/spree/paybox_system_gateway_configuration.rb
+++ lib/spree/paybox_system_gateway_configuration.rb
@@ -6,8 +6,11 @@ class Spree::PayboxSystemGatewayConfiguration < Spree::Preferences::Configuratio
   preference :hash, :string, :default => 'sha512'
   preference :currency, :string, :default => 'euro'
   preference :paybox_url, :string, :default => "https://preprod-tpeweb.paybox.com/cgi/MYchoix_pagepaiement.cgi"
-  preference :paybox_url_back_one, :string, :default => nil
+
+  preference :paybox_url_back_one, :string, :default => "http://localhost:3000/"
   preference :paybox_url_back_two, :string, :default => nil
+
+  preference :effectue, :string, :default => "http://localhost:3000/"
 
   preference :callback_values, :hash, :default => { amount:"m", ref:"r", auto:"a", error:"e", signature:"k" }
 

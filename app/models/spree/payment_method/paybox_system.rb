76e51ae11fb72360188cd9302000f7d9c89328a7
Alban Merino <amerino@dotgee.fr>
2015-01-23 10:45:32 +0100

    Fix actions payment_method
    fix issue where the actions would not send back an array

--- app/models/spree/payment_method/paybox_system.rb
+++ app/models/spree/payment_method/paybox_system.rb
@@ -1,7 +1,7 @@
 module Spree
   class PaymentMethod::PayboxSystem < PaymentMethod
     def actions
-      %{capture void}
+      %w{capture void}
     end
 
     # Indicates whether its possible to capture the payment

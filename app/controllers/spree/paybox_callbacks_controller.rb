1dd01267dbe3ac4b8103b8c42cfe575d7d2acbda
Alban Merino <amerino@dotgee.fr>
2015-02-03 11:15:18 +0100

    Fix ipn paybox

--- app/controllers/spree/paybox_callbacks_controller.rb
+++ app/controllers/spree/paybox_callbacks_controller.rb
@@ -8,34 +8,38 @@ class Spree::PayboxCallbacksController < Payr::BillsController
     @order = Spree::Order.find(params[:ref])
     if params[:error] == NO_ERROR #&& Payr::Client.new.check_response_ipn(request.url)
       unless @order.payments.where(:source_type => 'Spree::PayboxSystemTransaction').present?
-        payment_method = Spree::PaymentMethod.where(type: "Spree::PaymentMethod::PayboxSystem").first
+        set_payment
         paybox_transaction = Spree::PayboxSystemTransaction.create_from_postback params.merge(:action => 'paid')
-        payment = @order.payments.where(:state => 'checkout',
-                                        :payment_method_id => payment_method.id).first
-
-        if payment
-          payment.source = paybox_transaction
-          payment.save
+        if @payment
+          @payment.source = paybox_transaction
+          @payment.save
         else
-          payment = @order.payments.create(:amount => @order.total,
+          @payment = @order.payments.create(:amount => @order.total,
                                            :source => paybox_transaction,
-                                           :payment_method_id => payment_method.id)
+                                           :payment_method_id => @payment_method.id)
         end
 
-        payment.started_processing!
-        unless payment.completed?
-          payment.update_attributes(state: "checkout", response_code: "")
+        @payment.started_processing!
+        unless @payment.completed?
+          @payment.update_attributes(state: "checkout", response_code: "")
           # see: app/controllers/spree/skrill_status_controller.rb line 22
-          payment.complete!
+          @payment.complete!
         end
       end
 
       @order.finalize!
       logger.debug "PAYBOX_PAID: #{payment_method.inspect} #{@order.payments.inspect} #{@order.inspect} #{params.inspect}"
     else
-      payment.update_attributes(state: "invalid", response_code: params[:error])
+      set_payment
+      @payment.update_attributes(state: "invalid", response_code: params[:error]) if payment
       logger.debug "Erreur: #{params[:error]}"
     end
     render nothing: true, :status => 200, :content_type => 'text/html'
+  end
+
+  def set_payment
+    @payment_method = Spree::PaymentMethod.where(type: "Spree::PaymentMethod::PayboxSystem").first
+    @payment = @order.payments.where(:state => 'checkout',
+                                        :payment_method_id => payment_method.id).first
   end
 end
